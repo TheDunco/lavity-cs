4f278f8fef9001b5d63883a35f3442b0865421ee
Duncan Van Keulen <duncvan@gmail.com>
2025-09-16 22:44:28 -0400

    Reimplement character movement

--- Scenes/Creatures/Player.cs
+++ Scenes/Creatures/Player.cs
@@ -3,21 +3,85 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	[Export] private int Acceleration = 10;
-	private double Speed = 0;
+	[Export] private int Acceleration = 700;
+	[Export] private float AirResistance = 0.0002f;
+	[Export] private float MaxVelocity = 1000;
+
+	private Sprite2D Sprite = null;
 
 	public override void _Ready()
 	{
 		GD.Print("Ready");
+		Sprite = GetNode<Sprite2D>("Sprite");
+	}
+
+
+	private void OrientByRotation()
+	{
+
+		var CurScaleY = Scale.Y;
+		double rotationCos = Math.Cos(Rotation);
+		if (rotationCos < 0 && CurScaleY > 0)
+		{
+
+			Scale = new Vector2(Scale.X, -Scale.Y);
+		}
+		else if (rotationCos > 0 && CurScaleY < 0)
+		{
+
+			Scale = new Vector2(Scale.X, Math.Abs(Scale.Y));
+		}
 	}
 
 	public override void _Process(double delta)
 	{
-		if (Input.IsKeyPressed(Key.W))
+
+		bool InputAdded = false;
+		if (Input.IsActionPressed("MoveUp"))
 		{
-			Speed += Acceleration * delta * 0.5;
-			Position += Vector2.Right * (float)(Speed * delta);
-			Speed += Acceleration * delta * 0.5;
+			Velocity += Vector2.Up * Acceleration * (float)delta;
+			InputAdded = true;
 		}
+
+		if (Input.IsActionPressed("MoveLeft"))
+		{
+			Velocity += Vector2.Left * Acceleration * (float)(delta);
+			InputAdded = true;
+		}
+
+		if (Input.IsActionPressed("MoveDown"))
+		{
+			Velocity += Vector2.Down * Acceleration * (float)(delta);
+			InputAdded = true;
+		}
+
+		if (Input.IsActionPressed("MoveRight"))
+		{
+			Velocity += Vector2.Right * Acceleration * (float)(delta);
+			InputAdded = true;
+		}
+
+		if (Input.IsKeyPressed(Key.Space))
+		{
+			Position = Vector2.Zero;
+		}
+
+		bool isZeroApprox = Velocity.IsZeroApprox();
+
+		if (!isZeroApprox && !InputAdded)
+		{
+			Velocity += -Velocity * AirResistance;
+		}
+		else if (isZeroApprox)
+		{
+			Velocity = Vector2.Zero;
+		}
+
+		Velocity = Velocity.Clamp(-MaxVelocity, MaxVelocity);
+
+		LookAt(Velocity.Normalized() + Position);
+		OrientByRotation();
+
+		MoveAndSlide();
 	}
 }
